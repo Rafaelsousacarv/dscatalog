6327c71f87a4917a4a8b0d30c9e390de2f2ad3bd
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2023-04-02 17:55:26 -0300

    Catalog - ProductDetails Link

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,30 +1,39 @@
-import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg'
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 
-import './styles.css'
+import './styles.css';
+import { Link } from 'react-router-dom';
 const ProductDetails = () => {
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
-        <div className="goback-container">
+        <Link to="/products">
+          <div className="goback-container">
             <ArrowIcon />
             <h2>VOLTAR</h2>
-        </div>
+          </div>
+        </Link>
         <div className="row ">
           <div className="col-xl-6">
-                <div className="img-container">
-                    <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="" />
-                </div>
-                <div className="name-price-container">
-                    <h1>Nome do produto</h1>
-                    <ProductPrice price={2345.45}/>
-                </div>
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt=""
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.45} />
+            </div>
           </div>
           <div className="col-xl-6">
-                <div className="description-container">
-                    <h2>Descrição do Produto</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatum?</p>
-                </div>
+            <div className="description-container">
+              <h2>Descrição do Produto</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium, voluptatum?
+              </p>
+            </div>
           </div>
         </div>
       </div>
