815c3a0c4e08011fe0e2b31c968b5f42fbede353
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2023-04-04 11:27:07 -0300

    Auth routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
+import Auth from 'pages/Admin/Auth';
 import ProductDetails from 'pages/ProductDetails';
 
 const Routes = () => (
@@ -17,6 +18,10 @@ const Routes = () => (
       </Route>
       <Route path="/products/:productId">
         <ProductDetails />
+      </Route>
+      <Redirect from="/admin/auth" to="/admin/auth/login" exact/>      
+      <Route path="/admin/auth"> 
+        <Auth />
       </Route>
       <Redirect from="/admin" to="/admin/products" exact/>
       <Route path="/admin">
