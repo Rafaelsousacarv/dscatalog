11b51f52045f1a90b1a7fbdbd08394b5fae51521
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2023-04-09 13:27:23 -0300

    Authorized request

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -6,8 +6,8 @@ import { Product } from 'types/product';
 import Pagination from 'components/Pagination';
 import { useEffect, useState } from 'react';
 import { SpringPage } from 'types/vendor/spring';
-import axios, { AxiosRequestConfig } from 'axios';
-import { BASE_URL } from 'util/requests';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 import CardLoarder from './CardLoarder';
 
 const Catalog = () => {
@@ -17,7 +17,6 @@ const Catalog = () => {
     const params: AxiosRequestConfig = {
       method: 'GET',
       url: `/products`,
-      baseURL: BASE_URL,
       params: {
         page: 0,
         size: 12,
@@ -26,7 +25,7 @@ const Catalog = () => {
 
     setIsLoading(true);
 
-    axios(params)
+    requestBackend(params)
       .then((responde) => {
         setPage(responde.data);
       })
